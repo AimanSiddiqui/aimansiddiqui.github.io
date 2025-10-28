118b50ad34f3382c051091a360caf1f77035280a
AimanSiddiquiSAAR <aisi00001@stud.uni-saarland.de>
2025-10-28 03:00:09 +0100

    Fix: Update Vite configuration and entry point

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    minify: true,
-    cssCodeSplit: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
     }
   }
 })
