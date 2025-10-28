ce154320f14451d84e5b3f8d7a493e1c494959e0
AimanSiddiquiSAAR <aisi00001@stud.uni-saarland.de>
2025-10-28 03:53:26 +0100

    Simplify deployment configuration

--- source/vite.config.js
+++ source/vite.config.js
@@ -4,19 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  root: '.',
-  publicDir: '../public',
+  base: '/',
+  root: './',
   build: {
     outDir: '../',
     emptyOutDir: false,
     rollupOptions: {
-      input: './src/main.js',
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
+      input: './src/main.js'
     }
   }
 })
